d1eec08748a3dd804ca55f17fb7ef2846777b834
faithsylvia1 <you@example.com>
2021-05-12 10:23:37 +1000

    work on airline model

--- db/migrate/20210511064836_create_airplanes.rb
+++ db/migrate/20210511064836_create_airplanes.rb
@@ -1,6 +1,12 @@
 class CreateAirplanes < ActiveRecord::Migration[5.2]
   def change
     create_table :airplanes do |t|
+      t.text :airline
+      t.text :name
+      t.integer :column
+      t.integer :row
+      t.text :seats, default: [], array: true
+      t.timestamps
     end
   end
 end
